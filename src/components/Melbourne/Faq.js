a96b5dd623078cbe54f32d22be03dbf1752e4f41
Jay <141212189+jayx2u@users.noreply.github.com>
2024-10-03 20:03:26 +1000

    Updated contact information

--- src/components/Melbourne/Faq.js
+++ src/components/Melbourne/Faq.js
@@ -32,7 +32,7 @@ const faqData = [
     question: "Am I eligible to sign up?",
     answer: `If you're 18 or under, yes, we are so excited to see you! If you're over 18 but
         still in high school, shoot us an email at
-        <a href="mailto:counterspell@hackclub.com">counterspell@hackclub.com</a>.`,
+        <a href="mailto:contact@counterspellmelbourne.com">contact@counterspellmelbourne.com</a>.`,
   },
   {
     question: "Does participating cost anything?",
@@ -66,9 +66,9 @@ const faqData = [
   {
     question: "How can I reach out for more questions?",
     answer: `Contact us! Reach out at
-          <a href="https://hackclub.com/slack/">#counterspell</a>
+          <a href="https://hackclub.com/slack/">#counterspell-melbourne</a>
           on the Hack Club Slack or email us at
-          <a href="mailto:counterspell@hackclub.com">counterspell@hackclub.com</a>.
+          <a href="mailto:contact@counterspellmelbourne.com">contact@counterspellmelbourne.com</a>.
           We're always ready to answer all your questions!`,
   },
 ];
